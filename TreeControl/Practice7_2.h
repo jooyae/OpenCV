a57fd7f185646df0ba6a34462a104508083486d4
JooYae <jooyae3970@naver.com>
2021-06-22 17:16:10 +0900

    Tree Control

---
+++ TreeControl/Practice7_2.h
@@ -0,0 +1,32 @@
+
+// Practice7_2.h : PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CPractice7_2App:
+// 이 클래스의 구현에 대해서는 Practice7_2.cpp을 참조하십시오.
+//
+
+class CPractice7_2App : public CWinApp
+{
+public:
+	CPractice7_2App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPractice7_2App theApp;
