39c452c12f93f798e3b8d63ee88befd7fa8c064d
JooYae <jooyae3970@naver.com>
2021-06-22 12:50:05 +0900

    OpenCV graphic

---
+++ Graphic/Practice11_1/Practice11_1.h
@@ -0,0 +1,32 @@
+
+// Practice11_1.h : PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CPractice11_1App:
+// 이 클래스의 구현에 대해서는 Practice11_1.cpp을 참조하십시오.
+//
+
+class CPractice11_1App : public CWinApp
+{
+public:
+	CPractice11_1App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPractice11_1App theApp;
